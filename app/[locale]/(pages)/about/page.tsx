23c67bbb9d54d2a20d917af0500776b901c254c0
Mohammad Wahid <mohammadwahid.eng@gmail.com>
2025-02-11 16:05:12 +0600

    page meta translation

--- app/[locale]/(pages)/about/page.tsx
+++ app/[locale]/(pages)/about/page.tsx
@@ -1,7 +1,15 @@
 import { useTranslations } from "next-intl";
-import { setRequestLocale } from "next-intl/server";
-export default function About({ params: { locale } }: { params: { locale: string } }) {
-  setRequestLocale(locale);
+import { getTranslations } from "next-intl/server";
+
+export async function generateMetadata({ params: { locale } }: { params: { locale: string } }) {
+  const t = await getTranslations({ locale, namespace: "AboutPage" });
+  return {
+    title: t("meta.title"),
+    description: t("meta.description"),
+  };
+}
+
+export default function About() {
   const t = useTranslations("AboutPage");
   return <div>{t("title")}</div>;
 }
--- app/[locale]/(pages)/contact/page.tsx
+++ app/[locale]/(pages)/contact/page.tsx
@@ -1,7 +1,15 @@
 import { useTranslations } from "next-intl";
-import { setRequestLocale } from "next-intl/server";
-export default function Contact({ params: { locale } }: { params: { locale: string } }) {
-  setRequestLocale(locale);
+import { getTranslations } from "next-intl/server";
+
+export async function generateMetadata({ params: { locale } }: { params: { locale: string } }) {
+  const t = await getTranslations({ locale, namespace: "ContactPage" });
+  return {
+    title: t("meta.title"),
+    description: t("meta.description"),
+  };
+}
+
+export default function Contact() {
   const t = useTranslations("ContactPage");
   return <div>{t("title")}</div>;
 }
